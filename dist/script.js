e62b92d5b474c6fab21caa11bcf2333fd228405b
leoportogtr86 <leoporto_studio@hotmail.com>
2021-01-30 07:00:58 -0300

    configurando pra criar a pasta dist

--- dist/script.js
+++ dist/script.js
@@ -2,3 +2,4 @@
 //comandos=>
 //npm start (live-server) starta o projeto e abre o servidor
 //tsc -w (monitora e compila os arquivos ts para js)
+console.log('hello typescript');
--- introducao/script.ts
+++ introducao/script.ts
@@ -1,3 +1,5 @@
 //comandos=>
 //npm start (live-server) starta o projeto e abre o servidor
 //tsc -w (monitora e compila os arquivos ts para js)
+
+console.log('hello typescript')
