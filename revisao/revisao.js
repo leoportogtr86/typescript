f7cc1b07a142cd84709a52055217013e737929c7
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-26 14:31:20 -0300

    revisao

---
+++ revisao/revisao.js
@@ -0,0 +1,8 @@
+var nomeCompleto = "Leonardo Porto";
+var salario = 2600;
+var profissao = "dev fullstack";
+var tecnologias = ["javascript", "html", "css", "typescript", "angular", "react"];
+console.log(nomeCompleto);
+console.log(salario);
+console.log(profissao);
+console.log(tecnologias);
---
+++ revisao/revisao.ts
@@ -0,0 +1,12 @@
+let nomeCompleto: string = "Leonardo Porto"
+let salario: number = 2600
+let profissao: string = "dev fullstack"
+let tecnologias: string[] = ["javascript", "html", "css", "typescript", "angular", "react"]
+
+
+console.log(nomeCompleto)
+console.log(salario)
+console.log(profissao)
+console.log(tecnologias)
+
+
