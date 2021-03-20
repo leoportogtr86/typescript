65722ab104a60477af86d6b86c9e8b252ef11431
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-20 09:57:24 -0300

    generics 1

---
+++ generics/generics.ts
@@ -0,0 +1,14 @@
+let nomes: Array<string> =  ["Juca", "Lena", "Mauro", "Nina"]
+
+
+function imprimeNomes(nomes: Array<string>): void {
+
+
+    nomes.forEach((e)=>{
+
+        console.log(e)
+    })
+    
+}
+
+imprimeNomes(nomes)
